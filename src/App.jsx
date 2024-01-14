52a31b08905dcf25ab7a414fb4a1f3649152e07c
Savannah Lyles <savannahhlyles513@gmail.com>
2024-01-13 22:29:31 -0500

    Fixing npm run dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, HashRouter } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar';
+import NavigationBar from './NavigationBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Portfolio.css';
-import Footer from './Footer';
+import Footer from "./Footer";
 
 export default function Portfolio() {
     return (
