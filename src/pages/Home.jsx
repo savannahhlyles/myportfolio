b7ac5ac67cee900bf078d04871cc4ed504b6ef5b
Savannah Lyles <savannahhlyles513@gmail.com>
2024-01-29 18:08:39 -0500

    Home animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,44 +14,52 @@ export default function Home() {
             duration: 800,
             easing: 'ease-out',
         });
+
+        // Add event listener for scroll
+        window.addEventListener('scroll', AOS.refresh);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('scroll', AOS.refresh);
+        };
     }, []);
 
     return (
         <>
-  <div className="flexbox-container">
-    <NavigationBar />
-    <div id="box1" className="color-block" data-aos="fade-right"></div>
-    <div id="box2" className="color-block" data-aos="fade-left"></div>
-    <div id="content-container" data-aos="fade-up">
-      <h1>Savann<span className="color-change">ah</span></h1>
-      <h1>Lyles</h1>
-    </div>
-  </div>
+            <div className="flexbox-container">
+                <NavigationBar />
+                <div id="box1" className="color-block" data-aos="fade-right"></div>
+                <div id="box2" className="color-block" data-aos="fade-left"></div>
+                <div id="content-container" data-aos="fade-up">
+                    <h1>Savann<span className="color-change">ah</span></h1>
+                    <h1>Lyles</h1>
+                </div>
+            </div>
 
-  <div id="boxes">
-    <div id="boxa" className="color-block"></div>
-    <div id="boxb" className="color-block"></div>
-    <div id="boxc" className="color-block"></div>
-  </div>
+            <div id="boxes">
+                <div id="boxa" className="color-block"></div>
+                <div id="boxb" className="color-block"></div>
+                <div id="boxc" className="color-block"></div>
+            </div>
 
-  <div id="pictures">
-    <img src={crown} alt="Crown Image" id="crown"/>
-    <img src={coding} alt="Coding Image" id="coding"/>
-    <img src={resume} alt="Resume Image" id="resume"/>
-  </div>
+            <div id="pictures">
+                <img src={crown} alt="Crown Image" id="crown" data-aos="fade-up" data-aos-delay="500"/>
+                <img src={coding} alt="Coding Image" id="coding" data-aos="fade-up" data-aos-delay="500"/>
+                <img src={resume} alt="Resume Image" id="resume" data-aos="fade-up" data-aos-delay="500"/>
+            </div>
 
- <div id="blurb-container">
-    <div id="firstb">Undergraduate at <br />Columbia Engineering <br />studying computer science.</div>
-    <div id="secondb">Interests in < br /> full-stack SWE, design, < br /> and entrepreneurship.</div>
-    <div id="thirdb">Open to new < br /> opportunities!</div>
-  </div>
-  <div className="button-container">
-    <button className="button">
-      <span>Resume</span>
-    </button>
-  </div>
-  <Footer />
-</>
+            <div id="blurb-container" data-aos="fade-up" data-aos-delay="800">
+                <div id="firstb">Undergraduate at <br />Columbia Engineering <br />studying computer science.</div>
+                <div id="secondb">Interests in < br /> full-stack SWE, design, < br /> and entrepreneurship.</div>
+                <div id="thirdb">Open to new < br /> opportunities!</div>
+            </div>
 
+            <div className="button-container">
+                <button className="button" data-aos="fade-up" data-aos-delay="800">
+                    <span>Resume</span>
+                </button>
+            </div>
+            <Footer />
+        </>
     );
 }
