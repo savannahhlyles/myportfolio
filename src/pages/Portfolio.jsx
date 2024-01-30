99232e0a56c4f85841e8a32b05409d449fd8e6a3
Savannah Lyles <savannahhlyles513@gmail.com>
2024-01-29 19:42:09 -0500

    Portfolio changes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import './Portfolio.css';
-import Footer from "../Footer";
+import Footer from '../Footer';
 import NavigationBar from '../NavigationBar';
+import stocks from '../images/stocks.avif';
+import laptop from '../images/coding.jpg';
 
 export default function Portfolio() {
-    return (
-        <div className="flexbox-container">
-            <NavigationBar />
-            <p>portfolio</p>
-            <Footer />
+  return (
+    <div className="flexbox-container">
+      <NavigationBar />
+      <div className="timeline">
+        <div className="container left">
+          <div className="content">
+            <h1>February 2024</h1>
+              <h2>Project Name</h2>
+              <p>
+              Project description.
+            </p>
+          </div>
         </div>
-    );
+        <div className="container right">
+          <div className="content">
+            <h1>January 2024</h1>
+            <h2><a href="https://savannahhlyles.github.io/myportfolio/" target="_blank">Portfolio Website</a></h2>
+            <p>
+            Designed and developed this website, employing HTML, CSS, and JavaScript for implementation, while utilizing GitHub for version control and hosting.
+            </p>
+          </div>
+        </div>
+        <div className="container left">
+          <div className="content">
+            <h1>December 2023</h1>
+            <h2><a href="https://github.com/savannahhlyles/stock-price-prediction" target="_blank">Stock Price Prediction With Random Forest Regression</a></h2>
+            <p>
+            Developed and implemented a stock price prediction system utilizing Random Forest Regression, showcasing expertise in machine learning, data processing, technical analysis, hyperparameter tuning, and data visualization.
+            </p>
+          </div>
+        </div>
+        <div className="container right">
+          <div className="content">
+            <h1>November 2023</h1>
+            <h2><a href="https://github.com/savannahhlyles/epidemic-simulation" target="_blank">Epidemic Simulation Using Cellular Automaton Infection Model</a></h2>
+            <p>
+            Developed and implemented a Python-based epidemic simulation using cellular automaton principles, showcasing strong skills in Python programming, simulation modeling, algorithm design, and data visualization.
+            </p>
+          </div>
+        </div>
+
+        <img src={stocks} alt="Stock Market Image" className="stocks" width="500"/>
+      </div>
+      <Footer />
+    </div>
+  );
 }
